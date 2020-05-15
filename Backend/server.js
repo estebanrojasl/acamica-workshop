da2c6e3f1c6efd46d8aa771fe70b2ee356b110be
esteban7590 <esteban7590@hotmail.com>
2020-05-14 20:42:12 -0500

    Definiendo respuestas de rutas

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ventas = require("sales.json").ventas;
+const paquetes = require("tourPackage.json");
+const compradores = require("user.json").customers;
 
 const server = express();
 const PORT = 3000;
@@ -8,7 +11,7 @@ server.use(bodyParser.json());
 
 // chequear estado API
 server.get("/api/v1/turisteo/health", (req, res) => {
-    return res.status(200).json({ status: "OK" });
+  return res.status(200).json({ status: "OK" });
 });
 // listar todos los paquetes
 server.get("/api/v1/turisteo/paquetes", (req, res) => {});
@@ -24,10 +27,24 @@ server.post("/api/v1/turisteo/compra", (req, res) => {});
 
 //----------------------------------------------------------
 // listar todos los paquetes por id de usuario
-server.get("/api/v1/turisteo/paquetes/:id", (req, res) => {});
+server.get("/api/v1/turisteo/paquetes/:id", (req, res) => {
+  const comprador = compradores.find(
+    (element) => element.id === parseInt(req.params.id)
+  );
+  if (!comprador) {
+    res.status(404).json({ mensaje: "Comprador inexistente" });
+  } else {
+    const paquetesComprador = ventas.find(
+      (element) => element.idComprador === parseInt(req.params.id)
+    );
+    res.status(200).json(paquetesComprador);
+  }
+});
 
 // listar todos los paquetes comprados
-server.get("/api/v1/turisteo/paquetes/ventas", (req, res) => {});
+server.get("/api/v1/turisteo/paquetes/ventas", (req, res) => {
+  res.status(200).json(ventas);
+});
 
 // Crear un paquete
 server.post("/api/v1/turisteo/paquetes", (req, res) => {});
@@ -39,5 +56,5 @@ server.put("/api/v1/turisteo/paquetes/:id", (req, res) => {});
 server.delete("/api/v1/turisteo/paquetes/:id", (req, res) => {});
 
 server.listen(PORT, () => {
-    console.log("Servidor escuchando en puerto: " + PORT);
+  console.log("Servidor escuchando en puerto: " + PORT);
 });
