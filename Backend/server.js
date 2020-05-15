204d5b1d7954974a5e32cf7ed8161cbb7b460ef6
esteban7590 <esteban7590@hotmail.com>
2020-05-14 21:01:53 -0500

    respuestas de rutas

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ventas = require("sales.json").ventas;
-const paquetes = require("tourPackage.json");
-const compradores = require("user.json").customers;
+const ventas = require("./entities/sales.json").ventas;
+const paquetes = require("./entities/tourPackage.json").tourPackages;
+const compradores = require("./entities/user.json").customers;
 
 const server = express();
 const PORT = 3000;
@@ -26,6 +26,11 @@ server.post("/api/v1/turisteo/login", (req, res) => {});
 server.post("/api/v1/turisteo/compra", (req, res) => {});
 
 //----------------------------------------------------------
+// listar todos los paquetes comprados
+server.get("/api/v1/turisteo/paquetes/ventas", (req, res) => {
+  res.status(200).json(ventas);
+});
+
 // listar todos los paquetes por id de usuario
 server.get("/api/v1/turisteo/paquetes/:id", (req, res) => {
   const comprador = compradores.find(
@@ -41,19 +46,44 @@ server.get("/api/v1/turisteo/paquetes/:id", (req, res) => {
   }
 });
 
-// listar todos los paquetes comprados
-server.get("/api/v1/turisteo/paquetes/ventas", (req, res) => {
-  res.status(200).json(ventas);
+// Crear un paquete
+server.post("/api/v1/turisteo/paquetes", (req, res) => {
+  const { id } = req.body;
+  const idExiste = paquetes.filter((element) => element.id === id);
+  if (idExiste.length > 0) {
+    return res.status(409).json({ mensaje: "Ya existe ese id" });
+  }
+  const nuevoPaquete = req.body;
+  paquetes.push(nuevoPaquete);
+  return res.status(201).json(nuevoPaquete);
 });
 
-// Crear un paquete
-server.post("/api/v1/turisteo/paquetes", (req, res) => {});
+// Actualizar un paquete
+server.put("/api/v1/turisteo/paquetes/:id", (req, res) => {
+  const idPaquete = req.params.id;
+  const indexPaquete = paquetes.findIndex(
+    (element) => element.id === parseInt(idPaquete)
+  );
+  if (indexPaquete > -1) {
+    const paquete = req.body;
+    paquetes[indexPaquete] = paquete;
+    return res.status(200).json(paquetes[indexPaquete]);
+  }
+  return res.status(404).json({ mensaje: "Paquete inexistente" });
+});
 
 // Actualizar un paquete
-server.put("/api/v1/turisteo/paquetes/:id", (req, res) => {});
-
-// Actualizar un paquete
-server.delete("/api/v1/turisteo/paquetes/:id", (req, res) => {});
+server.delete("/api/v1/turisteo/paquetes/:id", (req, res) => {
+  const idPaquete = req.params.id;
+  const indexPaquete = paquetes.findIndex(
+    (element) => element.id === parseInt(idPaquete)
+  );
+  if (indexPaquete > -1) {
+    paquetes.splice(indexPaquete, 1);
+    return res.status(204).json();
+  }
+  return res.status(404).json({ mensaje: "Paquete inexistente" });
+});
 
 server.listen(PORT, () => {
   console.log("Servidor escuchando en puerto: " + PORT);
